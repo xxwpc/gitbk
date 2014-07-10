b6be1d1468e615ceea761abd4687d738bfb41111
肖选文 <xxw_pc@163.com>
2014-07-10 17:12:07 +0800

    修正 gen-version.sh 不能输出换行的错误

--- src/gen-version.sh
+++ src/gen-version.sh
@@ -8,11 +8,11 @@ hash="$(git log --pretty=format:%h -1 --abbrev=7)"
 
 #echo "#define VERSION_HASH \"$var\""
 
-ver_str="// This file is auto generated.\n// Don't edit.\n\n\n#define VERSION_DATE \"$date\"\n#define VERSION_HASH \"$hash\""
+ver_str="// This file is auto generated.\n// Don't edit.\n\n\n#define VERSION_DATE \"$date\"\n\n#define VERSION_HASH \"$hash\""
 
 
 if [ "$(echo $ver_str)" != "$(cat version.h 2> /dev/null)" ]; then
-   echo $ver_str > version.h
+   echo -e $ver_str > version.h
    echo "New version.h is generated => '$date $hash...'"
 fi
 
